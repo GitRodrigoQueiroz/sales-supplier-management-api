dd3f63fc069429f5dec296beb2d4026bb7c3e0c4
Rodrigo Queiroz <rodrigo.queiroz@delfosim.com>
2025-03-18 16:14:14 -0300

    name fixes

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,7 @@
+from backend.app.models.base import Base
+from backend.app.models.location import Location
+from backend.app.models.part import Part
+from backend.app.models.purchase import Purchase
+from backend.app.models.supplier import Supplier
+from backend.app.models.vehicle import Vehicle
+from backend.app.models.warranty import Warranty
