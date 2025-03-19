bcdc52230ef02352efb0c8d5c163ea17a18598e4
Rodrigo Queiroz <rodrigo.queiroz@delfosim.com>
2025-03-19 15:32:33 -0300

    adding user orm object at __init__

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -3,5 +3,6 @@ from backend.app.models.location import Location
 from backend.app.models.part import Part
 from backend.app.models.purchase import Purchase
 from backend.app.models.supplier import Supplier
+from backend.app.models.user import User
 from backend.app.models.vehicle import Vehicle
 from backend.app.models.warranty import Warranty
