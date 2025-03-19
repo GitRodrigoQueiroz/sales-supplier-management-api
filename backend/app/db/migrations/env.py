a75ba0462f79ab287642b2a21284181f3bfbf3c8
Rodrigo Queiroz <rodrigo.queiroz@delfosim.com>
2025-03-19 15:30:38 -0300

    fixes on alembic files and locations

--- backend/app/db/migrations/env.py
+++ backend/app/db/migrations/env.py
@@ -1,8 +1,8 @@
 from logging.config import fileConfig
 
 from alembic import context
-from sqlalchemy import engine_from_config, pool
 
+from backend.app.db import create_db_engine, credentials_db
 from backend.app.models import Base
 
 # this is the Alembic Config object, which provides
@@ -11,18 +11,8 @@ config = context.config
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
-fileConfig(config.config_file_name)
-
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = Base.metadata
-
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
 
 try:
     from dotenv import find_dotenv, load_dotenv
@@ -32,10 +22,20 @@ try:
 except ModuleNotFoundError:
     pass
 
-url = config.get_main_option("sqlalchemy.url")
+
+# add your model's MetaData object here
+# for 'autogenerate' support
+# from myapp import mymodel
+target_metadata = Base.metadata
+# target_metadata = None
+
+# other values from the config, defined by the needs of env.py,
+# can be acquired:
+# my_important_option = config.get_main_option("my_important_option")
+# ... etc.
 
 
-def run_migrations_offline():
+def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
 
     This configures the context with just a URL
@@ -59,18 +59,19 @@ def run_migrations_offline():
         context.run_migrations()
 
 
-def run_migrations_online():
+def run_migrations_online() -> None:
     """Run migrations in 'online' mode.
 
     In this scenario we need to create an Engine
     and associate a connection with the context.
 
     """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
+    # connectable = engine_from_config(
+    #     config.get_section(config.config_ini_section, {}),
+    #     prefix="sqlalchemy.",
+    #     poolclass=pool.NullPool,
+    # )
+    connectable = create_db_engine(**credentials_db)
 
     with connectable.connect() as connection:
         context.configure(connection=connection, target_metadata=target_metadata)
